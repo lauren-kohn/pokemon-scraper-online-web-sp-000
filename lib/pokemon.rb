2b1944158cd76c5d3013ef6e4474e184b10cf30a
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-19 23:18:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -6,16 +6,13 @@ class Pokemon
     @id, @name, @type, @db = id, name, type, db 
   end 
   
-  def self.save 
-    if self.id
-      self.update
-    else
+  def self.save(name, type, db) 
       sql = <<-SQL 
         INSERT INTO pokemon (name, type)
         VALUES (?, ?)
       SQL
       @db.execute(sql, self.name, self.type)
-    end
+    
   end 
   
   def self.find
